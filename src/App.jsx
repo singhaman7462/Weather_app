1364684bbba82f09e7d7ed3b444c3a5c2dbbbcc7
Aman Kumar Singh <amans.ug21.ec@nitp.ac.in>
2024-09-08 00:57:46 +0530

    completed the left part of the UI with hard coded values

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import './App.css'
-import Temperature from './components/Temperature';
+import "./App.css";
+import Highlights from "./components/Highlights";
+import Temperature from "./components/Temperature";
 
 function App() {
-  
-
   return (
-    <div className='bg-[#1F213A] h-screen flex justify-center'>
-      <div className='mt-40 w-1/5 h-1/3'>
-      <Temperature/>
+    <div className="bg-[#1F213A] h-screen flex justify-center">
+      <div className="mt-40 w-1/5 h-1/3">
+        <Temperature />
       </div>
-      <div className='bg-blue-400 mt-40 w-1/3 h-1/3'>Right</div>
+      <div className="mt-40 w-1/3 h-1/3 p-10">
+      <h2 className="text-slate-200 text-2xl">Todays highlights</h2>
+      <Highlights/>
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
--- src/components/Temperature.jsx
+++ src/components/Temperature.jsx
@@ -21,7 +21,7 @@ function Temperature() {
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="white"
-            class="size-6"
+            className="w-6 h-6 text-transform scale-100 hover:scale-110 transition-transform duration-300 ease-in-out"
           >
             <path
               stroke-linecap="round"
@@ -35,6 +35,51 @@ function Temperature() {
             />
           </svg>
         </div>
+      </div>
+
+      <div className="flex justify-center mt-4">
+        {/* <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="w-12 h-12 text-yellow-300 text-transform scale-100 hover:scale-110 transition-transform duration-300 ease-in-out"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+          />
+        </svg> */}
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="w-10 h-10 text-slate-200 text-transform scale-100 hover:scale-110 transition-transform duration-300 ease-in-out"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+          />
+        </svg>
+      </div>
+
+      <div className="flex justify-center items-center text-slate-200 mt-1">
+        <p className="font-semibold text-[40px]">23</p>
+        <span className="text-[25px]">°C</span>
+      </div>
+
+      <div className="flex justify-center items-center text-slate-300 mt-4 text-[25px] text-transform scale-100 hover:scale-110 transition-transform duration-300 ease-in-out">
+        Mist
+      </div>
+
+      <div className="flex justify-center text-slate-400 mt-5 text-[15px]">
+        Todays date and location
       </div>
     </>
   );
