4efa90b380e4f6597a0ff6120a6768df4882aca9
Aman Kumar Singh <amans.ug21.ec@nitp.ac.in>
2024-09-09 00:28:24 +0530

    Created a component to be used to show the highlights of weather in right div

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,16 @@ import Temperature from "./components/Temperature";
 function App() {
   return (
     <div className="bg-[#1F213A] h-screen flex justify-center">
+
       <div className="mt-40 w-1/5 h-1/3">
         <Temperature />
       </div>
-      <div className="mt-40 w-1/3 h-1/3 p-10">
-      <h2 className="text-slate-200 text-2xl">Todays highlights</h2>
+
+      <div className="mt-40 w-1/3 h-1/3 p-10 grid grid-cols-2 gap-6">
+      <h2 className="text-slate-200 text-2xl col-span-2">Todays highlights</h2>
+      <Highlights/>
+      <Highlights/>
+      <Highlights/>
       <Highlights/>
       </div>
     </div>
