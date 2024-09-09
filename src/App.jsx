6002ed2d5228c35ec695ab87e0f4634a5a39ee21
Aman Kumar Singh <amans.ug21.ec@nitp.ac.in>
2024-09-10 00:36:23 +0530

    Completed the version 1 of project.The live weather app is ready.

--- src/App.jsx
+++ src/App.jsx
@@ -27,26 +27,57 @@ function App() {
   return (
     <div className="bg-[#1F213A] h-screen flex justify-center">
       <div className="mt-40 w-1/5 h-1/3">
-        {weatherData&&<Temperature
-          setCity={setCity}
-          stats={{
-            temp:weatherData.current.temp_c,
-            condition:weatherData.current.condition.text,
-            isDay:weatherData.current.is_day,
-            location:weatherData.location.name,
-            time:weatherData.location.localtime
-          }}
-        />}
+        {weatherData && (
+          <Temperature
+            setCity={setCity}
+            stats={{
+              temp: weatherData.current.temp_c,
+              condition: weatherData.current.condition.text,
+              isDay: weatherData.current.is_day,
+              location: weatherData.location.name,
+              time: weatherData.location.localtime,
+            }}
+          />
+        )}
       </div>
 
       <div className="mt-40 w-1/3 h-1/3 p-10 grid grid-cols-2 gap-6">
         <h2 className="text-slate-200 text-2xl col-span-2">
           Todays highlights
         </h2>
-        <Highlights />
-        <Highlights />
-        <Highlights />
-        <Highlights />
+        {weatherData && (
+          <>
+            <Highlights
+              stats={{
+                title: "Wind Status",
+                value: weatherData.current.wind_mph,
+                unit: "mph",
+                direction: weatherData.current.wind_dir,
+              }}
+            />
+            <Highlights
+              stats={{
+                title: "Humidity",
+                value: weatherData.current.humidity,
+                unit: "%",
+              }}
+            />
+            <Highlights
+              stats={{
+                title: "Visibility",
+                value: weatherData.current.vis_miles,
+                unit: "miles",
+              }}
+            />
+            <Highlights
+              stats={{
+                title: "Air Pressure",
+                value: weatherData.current.pressure_mb,
+                unit: "mb",
+              }}
+            />
+          </>
+        )}
       </div>
     </div>
   );
--- src/components/Temperature.jsx
+++ src/components/Temperature.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Temperature({setCity,stats}) {
+function Temperature({ setCity, stats }) {
   const handleCityChange = (e) => {
     setCity(e.target.value);
   };
@@ -22,7 +22,7 @@ function Temperature({setCity,stats}) {
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="white"
-            className="w-6 h-6 text-transform scale-100 hover:scale-110 transition-transform duration-300 ease-in-out"
+            className="w-6 h-6 text-transform scale-100 hover:scale-110 transition-transform duration-300 ease-in-out cursor-pointer"
           >
             <path
               stroke-linecap="round"
@@ -39,43 +39,45 @@ function Temperature({setCity,stats}) {
       </div>
 
       <div className="flex justify-center mt-8">
-        {/* <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          className="w-12 h-12 text-yellow-300 text-transform scale-100 hover:scale-110 transition-transform duration-300 ease-in-out"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-          />
-        </svg> */}
-
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          className="w-16 h-16 text-slate-200 text-transform scale-100 hover:scale-110 transition-transform duration-300 ease-in-out"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
-          />
-        </svg>
+        {stats.isDay == 1 ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-12 h-12 text-yellow-300 text-transform scale-100 hover:scale-110 transition-transform duration-300 ease-in-out cursor-pointer"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-16 h-16 text-slate-200 text-transform scale-100 hover:scale-110 transition-transform duration-300 ease-in-out cursor-pointer"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+            />
+          </svg>
+        )}
       </div>
 
-      <div className="flex justify-center items-center text-slate-200 mt-8">
+      <div className="flex justify-center items-center text-slate-200 mt-6">
         <p className="font-semibold text-[40px]">{stats.temp}</p>
         <span className="text-[25px]">°C</span>
       </div>
 
-      <div className="flex justify-center items-center text-slate-300 mt-8 text-[30px] text-transform scale-100 hover:scale-110 transition-transform duration-300 ease-in-out">
+      <div className="flex justify-center items-center text-slate-300 mt-6 text-[20px] text-transform scale-100 hover:scale-110 transition-transform duration-300 ease-in-out cursor-pointer">
         {stats.condition}
       </div>
 
